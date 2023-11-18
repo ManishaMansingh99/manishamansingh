aba37b4d2e44553d061a20f385b53fc3f862ad34
ManishaMansingh99 <mansinghmanisha2@gmail.com>
2023-11-19 01:19:07 +0530

    added timeline animations

--- js/work.js
+++ js/work.js
@@ -1,82 +1,150 @@
-// jQuery(document).ready(function($){
-// 	var $timeline_block = $('.cd-timeline-block');
+// GSAP with ScrollTrigger for .cd-timeline-content
+gsap.registerPlugin(ScrollTrigger);
 
-// 	$timeline_block.each(function(){
-// 		if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
-// 			$(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
-// 		}
-// 	});
+// Select all the .cd-timeline-content elements
+gsap.utils.toArray('.cd-timeline-content').forEach(block => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.cd-timeline-content',
+      start: 'top center', 
+      end: 'bottom center', 
+      scrub: true,
+      markers: false 
+    }
+  });
 
-	  
-// 	$(window).on('scroll', function(){
-// 		console.log('Scroll event fired')
-// 		$timeline_block.each(function(){
-// 			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
-// 				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-// 			}
-// 		});
-// 	});
-// });
-
-
-// jQuery(document).ready(function($){
-//     var $timeline_block = $('.cd-timeline-block');
-//     var $workSection = $('#work'); 
-
-//     function isWorkSectionVisible() {
-//         var workSectionOffset = $workSection.offset().top;
-//         var windowBottom = $(window).scrollTop() + $(window).height();
-//         return windowBottom >= workSectionOffset;
-//     }
-
-//     $timeline_block.each(function(){
-//         if($(this).offset().top > $(window).scrollTop() + $(window).height() * 0.75) {
-//             $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
-//         }
-//     });
-
-//     $(window).on('scroll', function(){
-		
-//         if (isWorkSectionVisible()) {
-// 			console.log('Scroll event fired')
-//             $timeline_block.each(function(){
-//                 if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden')) {
-//                     $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-//                 }
-//             });
-//         }
-//     });
-// });
-
-jQuery(document).ready(function($){
-    var $timeline_block = $('.cd-timeline-block');
-
-    // Debounce function to limit the rate at which a function can fire
-    function debounce(func, wait, immediate) {
-        var timeout;
-        return function() {
-            var context = this, args = arguments;
-            var later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            var callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        };
-    };
-
-    // Function to handle the actual scroll event
-    var checkTimelineBlocks = function() {
-		console.log('Scroll event fired')
-        $timeline_block.each(function(){
-            if($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.05 && $(this).find('.cd-timeline-img').hasClass('is-hidden')) {
-                $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-            }
-        });
-    };
-
-    // Applying debounce to the scroll event
-    $(window).on('scroll', debounce(checkTimelineBlocks, 50)); // Adjust the 50ms delay as needed
+  // Define the animation for the block
+  tl.from(block, {
+    x: -100, 
+    autoAlpha: 0, 
+    duration: 1
+  });
 });
+
+// Select all the .cd-timeline-content elements
+gsap.utils.toArray('.cd-timeline-content1').forEach(block => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.cd-timeline-content1',
+      start: 'top center', 
+      end: 'bottom center', 
+      scrub: true,
+      markers: false 
+    }
+  });
+
+  // Define the animation for the block
+  tl.from(block, {
+    x: 100, 
+    autoAlpha: 0, 
+    duration: 1
+  });
+});
+// Select all the .cd-timeline-content elements
+gsap.utils.toArray('.cd-timeline-content2').forEach(block => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.cd-timeline-content2',
+      start: 'top center', 
+      end: 'bottom center', 
+      scrub: true,
+      markers: false 
+    }
+  });
+
+  // Define the animation for the block
+  tl.from(block, {
+    x: -100, 
+    autoAlpha: 0, 
+    duration: 1
+  });
+});
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+console.log(e)
+})
+
+function raf(time) {
+lenis.raf(time)
+requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   // GSAP with ScrollTrigger for .cd-timeline-content and .cd-timeline-content1
+//   gsap.registerPlugin(ScrollTrigger);
+
+//   // Animate .cd-timeline-content blocks
+//   gsap.utils.toArray('.cd-timeline-content').forEach(block => {
+//     gsap.from(block, {
+//       scrollTrigger: {
+//         trigger: block,
+//         start: 'top center',
+//         end: 'bottom center',
+//         scrub: true,
+//         markers: true // for debugging only, remove for production
+//       },
+//       x: -100,
+//       autoAlpha: 0,
+//       duration: 1
+//     });
+//   });
+
+//   // Animate .cd-timeline-content1 blocks
+//   gsap.utils.toArray('.cd-timeline-content1').forEach(block => {
+//     gsap.from(block, {
+//       scrollTrigger: {
+//         trigger: block,
+//         start: 'top center',
+//         end: 'bottom center',
+//         scrub: true,
+//         markers: true // for debugging only, remove for production
+//       },
+//       x: 100,
+//       autoAlpha: 0,
+//       duration: 1
+//     });
+//   });
+
+//   // Initialize Lenis for smooth scrolling
+//   const lenis = new Lenis({
+//     // Add Lenis initialization parameters here if needed
+//   });
+
+//   function raf(time) {
+//     lenis.raf(time);
+//     ScrollTrigger.update(); // Ensure ScrollTrigger updates on each frame
+//     requestAnimationFrame(raf);
+//   }
+
+//   requestAnimationFrame(raf);
+
+//   // Update ScrollTrigger after Lenis scroll
+//   lenis.on('scroll', ScrollTrigger.update);
+
+//   // Refresh ScrollTriggers on load, resize, or orientation change
+//   ScrollTrigger.addEventListener("refreshInit", function() {
+//     lenis.resize(); // Update Lenis on ScrollTrigger refresh
+//   });
+
+//   window.addEventListener('load', function() {
+//     ScrollTrigger.refresh();
+//   });
+
+//   window.addEventListener('resize', function() {
+//     ScrollTrigger.refresh();
+//   });
+
+//   window.addEventListener('orientationchange', function() {
+//     ScrollTrigger.refresh();
+//   });
+// });
+
