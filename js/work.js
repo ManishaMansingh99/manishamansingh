574536fcf74bef62cb168955bc0cde656a5e4907
ManishaMansingh99 <147563328+ManishaMansingh99@users.noreply.github.com>
2023-11-20 23:35:09 +0530

    latest updates

--- js/work.js
+++ js/work.js
@@ -1,216 +1,13 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   gsap.registerPlugin(ScrollTrigger);
-
-//   function initAnimations() {
-//     ['.cd-timeline-content', '.cd-timeline-content1', '.cd-timeline-content2'].forEach((selector, index) => {
-//       gsap.utils.toArray(selector).forEach(block => {
-//         let tl = gsap.timeline({
-//           scrollTrigger: {
-//             trigger: block, // Use each block as its own trigger
-//             start: 'top center', // Adjust this as needed
-//             end: 'bottom center',
-//             scrub: true,
-//             markers: true
-//           }
-//         });
-
-//         tl.from(block, {
-//           x: index % 2 === 0 ? -100 : 100, 
-//           autoAlpha: 0, 
-//           duration: 1
-//         });
-//       });
-//     });
-//   }
-
-//   initAnimations();
-
-//   // Initialize Lenis for smooth scrolling
-//   const lenis = new Lenis();
-
-//   lenis.on('scroll', (e) => {
-//     console.log(e);
-//   });
-
-//   function raf(time) {
-//     lenis.raf(time);
-//     requestAnimationFrame(raf);
-//   }
-
-//   requestAnimationFrame(raf);
-
-//   // Resize event listener to refresh ScrollTrigger
-//   window.addEventListener('resize', () => {
-//     ScrollTrigger.refresh();
-//     initAnimations(); // Reinitialize animations as the layout might have changed
-//   });
-
-//   // Programmatically trigger a resize event after a short delay
-//   setTimeout(() => {
-//     window.dispatchEvent(new Event('resize'));
-//   }, 500); // Adjust the delay as needed
-// });
-
-
-
-
-
-
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-// // GSAP with ScrollTrigger for .cd-timeline-content
-// gsap.registerPlugin(ScrollTrigger);
-
-// // Select all the .cd-timeline-content elements
-// gsap.utils.toArray('.cd-timeline-content').forEach(block => {
-//   let tl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: '.cd-timeline-content',
-//       start: 'top center', 
-//       end: 'bottom center', 
-//       scrub: true,
-//       markers: false 
-//     }
-//   });
-
-//   // Define the animation for the block
-//   tl.from(block, {
-//     x: -100, 
-//     autoAlpha: 0, 
-//     duration: 1
-//   });
-// });
-
-// // Select all the .cd-timeline-content elements
-// gsap.utils.toArray('.cd-timeline-content1').forEach(block => {
-//   let tl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: '.cd-timeline-content1',
-//       start: 'top center', 
-//       end: 'bottom center', 
-//       scrub: true,
-//       markers: false 
-//     }
-//   });
-
-//   // Define the animation for the block
-//   tl.from(block, {
-//     x: 100, 
-//     autoAlpha: 0, 
-//     duration: 1
-//   });
-// });
-// // Select all the .cd-timeline-content elements
-// gsap.utils.toArray('.cd-timeline-content2').forEach(block => {
-//   let tl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: '.cd-timeline-content2',
-//       start: 'top center', 
-//       end: 'bottom center', 
-//       scrub: true,
-//       markers: false 
-//     }
-//   });
-
-//   // Define the animation for the block
-//   tl.from(block, {
-//     x: -100, 
-//     autoAlpha: 0, 
-//     duration: 1
-//   });
-// });
-
-// // const lenis = new Lenis()
-
-// // lenis.on('scroll', (e) => {
-// // console.log(e)
-// // })
-
-// // function raf(time) {
-// // lenis.raf(time)
-// // requestAnimationFrame(raf)
-// // }
-
-// // requestAnimationFrame(raf)
-
-//   // Resize event listener to refresh ScrollTrigger
-//   window.addEventListener('resize', () => {
-//     ScrollTrigger.refresh();
-//     initAnimations(); // Reinitialize animations as the layout might have changed
-//   });
-
-
-// })
-
-  // window.onload = function() {
-  //   // let tl = gsap.timeline({
-  //   //   scrollTrigger: {
-  //   //     trigger: '.cd-timeline-content',
-  //   //     start: 'top center',
-  //   //     end: 'bottom center',
-  //   //     scrub: true,
-  //   //     markers: true
-  //   //   }
-  //   // });
-
-  //   // tl.from('.cd-timeline-content', {
-  //   //   x: -100, 
-  //   //   autoAlpha: 0, 
-  //   //   duration: 1
-  //   // });
-
-  //   // let tl1 = gsap.timeline({
-  //   //   scrollTrigger: {
-  //   //     trigger: '.cd-timeline-content1',
-  //   //     start: 'top center',
-  //   //     end: 'bottom center',
-  //   //     scrub: true,
-  //   //     markers: true
-  //   //   }
-  //   // });
-
-  //   // tl1.from('.cd-timeline-content1', {
-  //   //   x: 100, 
-  //   //   autoAlpha: 0, 
-  //   //   duration: 1
-  //   // });
-
-  //   let tl2 = gsap.timeline({
-  //     scrollTrigger: {
-  //       trigger: '.cd-timeline-content2',
-  //       start: 'top center',
-  //       end: 'bottom center',
-  //       scrub: true,
-  //       markers: true
-  //     }
-  //   });
-
-  //   tl2.from('.cd-timeline-content2', {
-  //     x: -100, 
-  //     autoAlpha: 0, 
-  //     duration: 1
-  //   });
-  //   ScrollTrigger.refresh();
-  // };
-
-
-
-
-
-
-  window.onload = function() {
-
+window.onload = function() {
+// profile
     // Timeline for '.cd-timeline-content'
     let tl_profile = gsap.timeline({
       scrollTrigger: {
         trigger: '.left-section',
-        start: 'top center',
+        start: 'top bottom',
         end: '25% center',
         scrub: true,
-        markers: true
+        markers: false
       }
     });
 
@@ -223,8 +20,8 @@
     let tl_profile_header = gsap.timeline({
       scrollTrigger: {
         trigger: '.profile-contain',
-        start: 'top center',
-        end: 'bottom center',
+        start: 'top bottom',
+        end: '25% center',
         scrub: true,
         markers: false
       }
@@ -235,14 +32,14 @@
       autoAlpha: 0, 
       duration: 1
     });
-
+// work
     let tl = gsap.timeline({
       scrollTrigger: {
         trigger: '.cd-timeline-content',
-        start: 'top center',
-        end: '25% center',
+        start: 'top bottom',
+        end: 'bottom center',
         scrub: true,
-        markers: true
+        markers: false
       }
     });
 
@@ -256,10 +53,10 @@
     let tl1 = gsap.timeline({
       scrollTrigger: {
         trigger: '.cd-timeline-content1',
-        start: 'top center',
-        end: '25% center',
+        start: 'top bottom',
+        end: 'bottom center',
         scrub: true,
-        markers: true
+        markers: false
       }
     });
 
@@ -273,10 +70,10 @@
     let tl2 = gsap.timeline({
       scrollTrigger: {
         trigger: '.cd-timeline-content2',
-        start: 'top center',
+        start: 'top bottom',
         end: '25% center',
         scrub: true,
-        markers: true
+        markers: false
       }
     });
 
@@ -286,12 +83,28 @@
       duration: 1
     });
 
+// project
+let tl_project_title = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.fade-project',
+    start: '-50% center',
+    end: 'bottom center',
+    scrub: true,
+    markers: false
+  }
+});
+
+tl_project_title.from('.fade-project', {
+  y: -100, 
+  autoAlpha: 0, 
+  duration: 2
+});
 
     let tl_project = gsap.timeline({
       scrollTrigger: {
         trigger: '.fade-in',
         start: 'top center',
-        end: 'bottom center',
+        end: '25% center',
         scrub: true,
         markers: false
       }
@@ -300,7 +113,7 @@
     tl_project.from('.fade-in', {
       y: 100, 
       autoAlpha: 0, 
-      duration: 1
+      duration: 2
     });
 
     let tl_project_textl = gsap.timeline({
@@ -309,14 +122,14 @@
         start: 'top center',
         end: '25% center',
         scrub: true,
-        markers: true
+        markers: false
       }
     });
 
     tl_project_textl.from('.text-card-left .data', {
       x: -100, 
       autoAlpha: 0, 
-      duration: 1
+      duration: 2
     });
 
     let tl_project_textr = gsap.timeline({
@@ -325,15 +138,85 @@
         start: 'top center',
         end: '25% center',
         scrub: true,
-        markers: true
+        markers: false
       }
     });
 
     tl_project_textr.from('.text-card-right .data', {
       x: 100, 
       autoAlpha: 0, 
+      duration: 2
+    });
+
+    // skills
+    let tl_skill_title = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#skills h1',
+        start: '-50% center',
+        end: '25% center',
+        scrub: true,
+        markers: false
+      }
+    });
+
+    tl_skill_title.from('#skills h1', {
+      x: -100, 
+      autoAlpha: 0, 
       duration: 1
     });
+
+    let tl_skill = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.skill',
+        start: '-50% center',
+        end: '25% center',
+        scrub: true,
+        markers: false
+      }
+    });
+
+    // tl_skill.from('.skill', {
+    //   x: 100, 
+    //   autoAlpha: 0, 
+    //   duration: 1
+    // });
+    tl_skill.from('.skill', {
+      rotation: 180, // Rotate 45 degrees
+      x: 100, 
+      autoAlpha: 0, 
+      duration: 1
+    });
+
+    // contact
+    let tl_contact_title = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.contact-container h1',
+        start: 'top bottom',
+        end: 'bottom center',
+        scrub: true,
+        markers: false
+      }
+    });
+    tl_contact_title.to('.contact-container h1', {
+      x: 0, 
+      autoAlpha: 1, 
+      duration: 1
+    });
+
+  let tl_contact = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.soc',
+      start: 'top bottom',
+      end: 'bottom center',
+      scrub: true,
+      markers: false
+    }
+  });
+  tl_contact.from('.soc', {
+    y: -100, 
+    autoAlpha: 0, 
+    duration: 1
+  });
     // Refresh ScrollTrigger after the window has fully loaded
     ScrollTrigger.refresh();
   };
