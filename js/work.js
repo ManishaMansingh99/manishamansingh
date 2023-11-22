06b25140e3dc21532658f29a14b28b884d73a655
ManishaMansingh99 <147563328+ManishaMansingh99@users.noreply.github.com>
2023-11-22 20:20:52 +0530

    updated content

--- js/work.js
+++ js/work.js
@@ -5,7 +5,7 @@ window.onload = function() {
       scrollTrigger: {
         trigger: '.left-section',
         start: 'top bottom',
-        end: '25% center',
+        end: '35% center',
         scrub: true,
         markers: false
       }
@@ -21,7 +21,7 @@ window.onload = function() {
       scrollTrigger: {
         trigger: '.profile-contain',
         start: 'top bottom',
-        end: '25% center',
+        end: '35% center',
         scrub: true,
         markers: false
       }
@@ -33,11 +33,25 @@ window.onload = function() {
       duration: 1
     });
 // work
+let tl_head = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.work-container h1',
+    start: 'top center',
+    end: '35% center',
+    scrub: true,
+    markers: false
+  }
+});
+tl_head.from('.work-container h1', {
+  x: -100, 
+  autoAlpha: 0, 
+  duration: 1
+});
     let tl = gsap.timeline({
       scrollTrigger: {
         trigger: '.cd-timeline-content',
         start: 'top bottom',
-        end: 'bottom center',
+        end: '35% center',
         scrub: true,
         markers: false
       }
@@ -71,7 +85,7 @@ window.onload = function() {
       scrollTrigger: {
         trigger: '.cd-timeline-content2',
         start: 'top bottom',
-        end: '25% center',
+        end: '35% center',
         scrub: true,
         markers: false
       }
@@ -87,73 +101,113 @@ window.onload = function() {
 let tl_project_title = gsap.timeline({
   scrollTrigger: {
     trigger: '.fade-project',
-    start: '-50% center',
-    end: 'bottom center',
+    start: 'top bottom',
+    end: '35% center',
     scrub: true,
     markers: false
   }
 });
-
 tl_project_title.from('.fade-project', {
   y: -100, 
   autoAlpha: 0, 
   duration: 2
 });
 
-    let tl_project = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.fade-in',
-        start: 'top center',
-        end: '25% center',
-        scrub: true,
-        markers: false
-      }
-    });
+let tl_project_heading1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.text-card-left h2',
+    start: 'top bottom',
+    end: '35% center',
+    scrub: true,
+    markers: false
+  }
+});
+tl_project_heading1.from('.text-card-left h2', {
+  x: -100, 
+  autoAlpha: 0, 
+  duration: 1
+});
+let tl_project_heading2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.text-card-right h2',
+    start: 'top bottom',
+    end: '35% center',
+    scrub: true,
+    markers: false
+  }
+});
+tl_project_heading2.from('.text-card-right h2', {
+  x: 100, 
+  autoAlpha: 0, 
+  duration: 1
+});
 
-    tl_project.from('.fade-in', {
-      y: 100, 
-      autoAlpha: 0, 
-      duration: 2
-    });
+let tl_project_textl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.text-card-left .data',
+    start: 'top bottom',
+    end: '35% center',
+    scrub: true,
+    markers: false
+  }
+});
+tl_project_textl.from('.text-card-left .data', {
+  x: -100, 
+  autoAlpha: 0, 
+  duration: 2
+});
 
-    let tl_project_textl = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.text-card-left .data',
-        start: 'top center',
-        end: '25% center',
-        scrub: true,
-        markers: false
-      }
-    });
+let tl_project_textr = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.text-card-right .data',
+    start: 'top bottom',
+    end: '35% center',
+    scrub: true,
+    markers: false
+  }
+});
 
-    tl_project_textl.from('.text-card-left .data', {
-      x: -100, 
-      autoAlpha: 0, 
-      duration: 2
-    });
+tl_project_textr.from('.text-card-right .data', {
+  x: 100, 
+  autoAlpha: 0, 
+  duration: 2
+});
 
-    let tl_project_textr = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.text-card-right .data',
-        start: 'top center',
-        end: '25% center',
-        scrub: true,
-        markers: false
-      }
-    });
-
-    tl_project_textr.from('.text-card-right .data', {
-      x: 100, 
-      autoAlpha: 0, 
-      duration: 2
-    });
+let tl_project_li1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.text-card-left a',
+    start: 'top bottom',
+    end: '15% center',
+    scrub: true,
+    markers: false
+  }
+});
+tl_project_li1.from('.text-card-left a', {
+  y: 10, 
+  autoAlpha: 0, 
+  duration: 0.5
+});
+let tl_project_li2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.text-card-right a',
+    start: 'top bottom',
+    end: '15% center',
+    scrub: true,
+    markers: false
+  }
+});
+tl_project_li2.from('.text-card-right a', {
+  y: 10, 
+  autoAlpha: 0, 
+  duration: 0.5
+});
 
     // skills
     let tl_skill_title = gsap.timeline({
       scrollTrigger: {
         trigger: '#skills h1',
-        start: '-50% center',
-        end: '25% center',
+        start: 'top bottom',
+        end: '35% center',
         scrub: true,
         markers: false
       }
@@ -168,18 +222,12 @@ tl_project_title.from('.fade-project', {
     let tl_skill = gsap.timeline({
       scrollTrigger: {
         trigger: '.skill',
-        start: '-50% center',
-        end: '25% center',
+        start: 'top bottom',
+        end: '35% center',
         scrub: true,
         markers: false
       }
     });
-
-    // tl_skill.from('.skill', {
-    //   x: 100, 
-    //   autoAlpha: 0, 
-    //   duration: 1
-    // });
     tl_skill.from('.skill', {
       rotation: 180, // Rotate 45 degrees
       x: 100, 
@@ -197,9 +245,23 @@ tl_project_title.from('.fade-project', {
         markers: false
       }
     });
-    tl_contact_title.to('.contact-container h1', {
+    tl_contact_title.from('.contact-container h1', {
+      x: 100, 
+      autoAlpha: 0, 
+      duration: 1
+    });
+    let tl_contact_text = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.contact-container p',
+        start: 'top bottom',
+        end: 'bottom center',
+        scrub: true,
+        markers: false
+      }
+    });
+    tl_contact_text.from('.contact-container p', {
       x: 0, 
-      autoAlpha: 1, 
+      autoAlpha: 0, 
       duration: 1
     });
 
