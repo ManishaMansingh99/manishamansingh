d2fe84b3250fbec926cec6d3e1bc0bd6863c5803
ManishaMansingh99 <147563328+ManishaMansingh99@users.noreply.github.com>
2023-11-30 04:07:28 +0530

    v2 with mobile optimisations

--- js/project.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.project-card-sm .card-button').forEach(button => {
-    button.addEventListener('click', () => {
-      button.closest('.project-card-sm').classList.toggle('opened');
-    });
-  });
-  
